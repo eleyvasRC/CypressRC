e66edbe502da5ff976549a6efaecbdde18ee68e3
Miguel Cordova <mcordova@recursoconfiable.com>
2021-09-09 12:27:41 -0500

    Se sube prueba de herdez

--- cypress/integration/Eventos.js
+++ cypress/integration/Eventos.js
@@ -35,44 +35,33 @@ describe('CP Detonación de eventos', function()
             cy.get('.button-menu-mobile > .zmdi').click({ force: true })            
         })
     })
+    
+    //iIngreso Evento Entrada Generica02
+    it('Prueba detonación evento Entrada Generica2', function(){
 
-           it('Fin de Carga', function(){
-        
-        //ingersar a modulo de carga de pedidos
-        cy.get('.has_sub a[href*="END_LOAD"]').click({ force: true })
-        //busqueda del folio de carga actual
+        //Ingresar a evento Entrada Generica 2
+        cy.get('.has_sub a[href*="feature_key='+this.param.eventos.entrada_generica2+'"]').click({ force: true })
+       //busqueda del folio de viaje actual
         cy.get('.sorting_1').contains(this.param.inputs_entrada.shipment).should('be.visible')
-        //Clic en boton Detalle de viaje
-        cy.get(':nth-child(5) > .btn').should('be.visible').click()
-        cy.wait(3000)
-        //Validar Pedido contenga el # de viaje
-        cy.get('.card-order-header > h5').contains(this.param.Detalles_de_viaje.pedido).should('be.visible')//.should('be.visible',this.param.Detalles_de_viaje.pedido)//
-        //validar F/H cita de entrega
-        cy.get(':nth-child(3) > .order-point').contains(this.param.Detalles_de_viaje.Cita_de_entrega).should('be.visible')//should('be.visible',this.param.Detalles_de_viaje.Cita_de_entrega)//
-        //Cerrar detalles
-        cy.get('.fa-arrow-right').click({ force: true })
-        //Click en boton confirmar
+      //validar F/H cita de carga
+        cy.get('.odd > :nth-child(3)').contains(this.param.Detalles_de_viaje.Cita_de_carga).should('be.visible')//should('be.visible',this.param.Detalles_de_viaje.Cita_de_entrega)//
+      //Click en boton confirmar
         cy.get(':nth-child(6) > .btn').should('be.visible').click({ force: true })
-        //Validar contenga el el modal de confirmacion
-        cy.get('.confirmation-modal > .modal-dialog > .modal-content').should('be.visible')
+      
+      //Validar contenga el el modal de confirmacion
+        //cy.get('.confirmation-modal > .modal-dialog > .modal-content').should('be.visible')
         //Validar presente el viaje que se cargo en el modal
-        cy.get('.confirmation-modal > .modal-dialog > .modal-content > .modal-body').contains(this.param.inputs_entrada.shipment).should('be.visible') ////.should('be.visible','have.text', this.param.inputs_entrada.shipment)
-        //validar boton cancelar
-        cy.get('.cancel').should('be.visible').click({force: true})
-        cy.wait(2000)
+        //cy.get('.confirmation-modal > .modal-dialog > .modal-content > .modal-body').contains(this.param.inputs_entrada.shipment).should('be.visible') ////.should('be.visible','have.text', this.param.inputs_entrada.shipment)
+      
+      //validar boton cancelar
+        //cy.get('.cancel').should('be.visible').click({force: true})
+        //cy.wait(2000)
+    
         //validar nuevamente el boton confirmar viaje
         cy.get(':nth-child(6) > .btn').should('be.visible').click({ force: true })
-        //Validar nuevamente el popup y que presente el viaje que se cargo en el modal
-        cy.get('body > div.confirmation-modal.modal.fade.in > div > div').contains(this.param.inputs_entrada.shipment).should('be.visible')//.contains(this.param.inputs_entrada.shipment).should('be.visible')
-        //clic en el boton confirmar
-        cy.get('.confirm').should('be.visible').click({force: true})
-   })
-/*
-   it('Documentos', function(){
-       //ingersar a modulo de carga de Documentos
-       //Validar que contenga el viaje
-       //validar 
-
-   })
-*/
+       //Validar nuevamente el popup y que presente el viaje que se cargo en el modal
+    //    cy.get('body > div.confirmation-modal.modal.fade.in > div > div').contains(this.param.inputs_entrada.shipment).should('be.visible')//.contains(this.param.inputs_entrada.shipment).should('be.visible')
+    
+    })
+   
 })
